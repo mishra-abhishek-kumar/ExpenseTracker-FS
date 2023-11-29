838330f8c0ba6848c82477f0af4ec21b00efc2fd
mishra_abhishek_kumar <mishraabhishek0807@gmail.com>
2023-11-29 20:25:43 +0530

    Setting up DB and Expense Model

--- BackEnd/app.js
+++ BackEnd/app.js
@@ -4,13 +4,27 @@ const express = require('express');
 const dotenv = require('dotenv');
 dotenv.config({path: './.env'});
 
+//import required to parse JSON data as a POST request
+const bodyParser = require('body-parser');
+
+//imports required for DB connection and table creation
+const sequelize = require('./util/dbConnect');
+const Expense = require('./models/Expense'); //Without this table was not getting created
+
 const app = express();
+
+app.use(bodyParser.json());
 
 app.use('/', (req, res, next) => {
     console.log("Getting Response");
 });
 
 const PORT = process.env.PORT || 4001;
-app.listen(PORT, () => {
-    console.log("Listening on PORT:", PORT);
-})
+sequelize.sync()
+    .then(result => {
+        console.log(result);
+        app.listen(PORT, () => {
+            console.log("Listening on PORT:", PORT);
+        });
+    })
+    .catch(err => console.log(err));
